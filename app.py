6866526b04966972ed453a92055466e12510d578
johanaph37 <johanapenah@gmail.com>
2026-03-06 01:47:25 -0500

    setup: se inicia estructura de aplicación

---
+++ app.py
@@ -0,0 +1,9 @@
+import funciones
+
+def menu():
+    while True:
+        print("\n MENÚ")
+        print("1. Registrar Estudiante")
+        print("2. Registrar Notas")
+        print("3. Ver promedio")
+        print("4. Salir")
