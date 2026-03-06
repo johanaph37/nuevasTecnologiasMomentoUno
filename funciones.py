54a4c01f3c9a019bf855462ed28844e198f80a9a
johanaph37 <johanapenah@gmail.com>
2026-03-06 01:52:18 -0500

    feat: se agrega funciones de registrar estudiante y notas

---
+++ funciones.py
@@ -0,0 +1,30 @@
+from diccionario import estudiantes
+
+def registrar_estudiante():
+    id_estudiante = input("Ingrese el la identificación del estudiante: ")
+    nombre = input("Ingrese el nombre del estudiante: ")
+
+    estudiantes[id_estudiante] = {
+    "nombre": nombre,
+    "nota1": 0.0,
+    "nota2": 0.0
+
+    }
+
+    print(f"Estudiante {nombre} registrado con éxito")
+
+
+def registrar_notas():
+    id_busqueda = input("Ingrese la identificación de estudiante para registrar las notas: ")
+
+    if id_busqueda in estudiantes:
+        nota1 = float(input("Ingrese la nota 1: "))
+        nota2 = float(input("Ingrese la nota 2: "))
+
+        estudiantes [id_busqueda]["nota1"] = nota1
+        estudiantes [id_busqueda]["nota2"] = nota2
+
+        print("Notas actualizadas correctamente")
+    else:
+        print("El estudiante no se encuentra registrado")
+        
