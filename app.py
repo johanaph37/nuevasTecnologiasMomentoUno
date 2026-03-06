ab6ad68f8967217dfd2dee6af22ece92df5f4637
johanaph37 <johanapenah@gmail.com>
2026-03-06 01:57:21 -0500

    se integran funciones al menú de opciones

--- app.py
+++ app.py
@@ -7,3 +7,22 @@ def menu():
         print("2. Registrar Notas")
         print("3. Ver promedio")
         print("4. Salir")
+
+        opcion = input("Seleccione una opcion: ")
+
+        if opcion == "1":
+            funciones.registrar_estudiante()
+        elif opcion == "2":
+            funciones.registrar_notas()
+        elif opcion == "3":
+            funciones.ver_promedio()
+        elif opcion == "4":
+            print("Saliendo...")
+            break
+        else:
+            print("Opción incorrecta")
+
+
+
+if __name__ == "__main__":
+    menu()
